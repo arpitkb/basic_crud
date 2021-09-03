927a283c6b2b2526526ed4dadafef31104fdb5b8
arpitkb <bansal_851937@student.nitw.ac.in>
2021-09-04 01:09:35 +0530

    set npm start

--- index.js
+++ index.js
@@ -117,6 +117,8 @@ app.use((err, req, res, next) => {
     res.status(status).render('user/error', { err })
 })
 
-app.listen('3000', () => {
-    console.log('server running on port 3000')
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`server running on port ${port}`)
 })
